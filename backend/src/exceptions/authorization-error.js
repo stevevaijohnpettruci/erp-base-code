c5591c8d01e8cbfe9f8a7853bb909e43ec92802b
stevevaijohnpettruci <stevevai20242023@gmail.com>
2026-08-01 19:51:00 +0700

    chore: remove backend from fe branch

--- backend/src/exceptions/authorization-error.js
+++
@@ -1,9 +0,0 @@
-import ClientError from './client-error.js';
-
-class AuthorizationError extends ClientError {
-  constructor(message) {
-    super(message, 403);
-    this.name = 'AuthorizationError';
-  }
-}
-export default AuthorizationError;
--- backend/src/services/departments/validator/schema.js
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi';
-
-export const departmentSchema = Joi.object({
-  name: Joi.string().min(2).max(100).required(),
-  description: Joi.string().max(255).optional(),
-});
